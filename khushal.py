c4f495ce52b9a09e156652f26364ea00a6cb2fb7
khushal-24 <khushalshivade24@gmail.com>
2024-02-22 10:41:09 +0530

    hello

--- khushal.py
+++ khushal.py
@@ -1,1 +1,35 @@
-print("git")
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="system@123",
+    database="db1"
+)
+mycursor=mydb.cursor()
+
+
+mycursor.execute ("CREATE TABLE Customers (name VARCHAR(255), address VARCHAR(255)) ")
+
+sql= "INSERT INTO Customers (name,address) VALUES (%s, %s)"
+val=("John","Highway 21")
+mycursor.execute(sql,val)
+
+mydb.commit()
+
+'''sql= "INSERT INTO Customers (name,address) VALUES (%s,%s)"
+val= [
+    ('Peter','Lowstreet 4'),
+    ('Amy','Apple st 652'),
+    ('Hannah','Mountain 21'),
+    ('Michael','Valley 345'),
+    ('Sandy','Ocean blvd 2'),
+    ('Betty','Green Grass 1'),
+    ('Richard','Sky st 331'),
+    ('Susan','One way 98')
+]
+mycursor.executemany(sql,val)
+mydb.commit()
+
+mycursor.execute("SELECT * FROM Customers ")
+myresult= mycursor.fetchall()'''
